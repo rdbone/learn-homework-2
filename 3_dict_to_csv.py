add4dcd8d56c113202b4f184b256c3b0ee8c56bf
rdbone <baysir17@gmail.com>
2023-05-26 11:02:53 +0300

    Выполненная домашняя работа

--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -9,13 +9,28 @@
 2. Запишите содержимое списка словарей в файл в формате csv
 
 """
+import csv
 
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    workers = [
+        {'name': 'Иван', 'age': 18, 'job': 'Программист'},
+        {'name': 'Илья', 'age': 27, 'job': 'Дворник'},
+        {'name': 'Игорь', 'age': 52, 'job': 'Врач'},
+        {'name': 'Дантес', 'age': 26, 'job': 'Инженер'},
+        {'name': 'Сантос', 'age': 27, 'job': 'Комик'},
+        {'name': 'Сальвадор', 'age': 28, 'job': 'Фронтендер'},
+        {'name': 'Настя', 'age': 27, 'job': 'Лектор'}
+    ]
+    with open('workers.csv', 'w', encoding='utf-8') as f:
+        headers = ['name', 'age', 'job']
+        writer = csv.DictWriter(f, headers, delimiter=';')
+        writer.writeheader()
+        for worker in workers:
+            writer.writerow(worker)
 
 if __name__ == "__main__":
     main()
